c95847ebc79b262f601dffc52165269d55b4df47
Karan Lathiya <2810karanlathiya@gmail.com>
2023-09-10 00:23:53 +0530

    upto 4

---
+++ try/create.go
@@ -0,0 +1,23 @@
+package main
+import (
+   "fmt"
+   "os"
+)
+//create main function to execute the program
+func main() {
+   // Open the file for writing
+   file, errs := os.Create("myfile.txt")
+   if errs != nil {
+      fmt.Println("Failed to create file:", errs)
+      return
+   }
+   defer file.Close()
+
+   // Write the string "Hello, World!" to the file
+   _, errs = file.WriteString("Hello, World!")
+   if errs != nil {
+      fmt.Println("Failed to write to file:", errs) //print the failed message
+      return
+   }
+   fmt.Println("Wrote to file 'myfile.txt'.") //print the success message
+}
---
+++ try/stack.go
@@ -0,0 +1,40 @@
+package main
+import "fmt"
+
+type Stack []int   //stack
+
+// Push adds a value to the top of the stack.
+func (st *Stack) Push(v int) {
+   *st = append(*st, v)
+}
+
+// Pop removes and returns the top value from the stack.
+func (st *Stack) Pop() int {
+   if st.IsEmpty() {
+      return 0
+   }
+   top := (*st)[len(*st)-1]
+   *st = (*st)[:len(*st)-1]
+   return top
+}
+
+func (st *Stack) IsEmpty() bool {
+   return len(*st) == 0
+}
+func (st *Stack) Peek() int{
+	top := (*st)[len(*st)-1]
+	return top
+}
+func main() {
+   st := Stack{}
+   st.Push(1)
+   st.Push(2)
+   fmt.Println("The value popped from the stack is given as:")
+ 
+   fmt.Println(st.Peek())  
+   fmt.Println(st.Pop())
+   fmt.Println(st.Pop())
+
+   fmt.Println("Is the stack empty?")
+   fmt.Println(st.IsEmpty())
+}
